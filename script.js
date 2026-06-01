ab7d75b6a9ae85a27ebedeb0d6ba049a690e44fd
Abhijeet Mishra <abhijitmishra3089@gmail.com>
2026-06-01 22:53:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const style=document.createElement("style");
 style.innerText=`
-.grid-container{
+#grid-container{
 display:grid;
 grid-template-rows:repeat(3,1fr);
 grid-template-columns:repeat(3,1fr);
 gap:5px;
 padding:4px;
-/*background-color:black;*/
+// background-color:black;
 }
-.grid-item{
+#grid-item{
 	height:150px;
 	border:1px solid black;	
 }
@@ -31,34 +31,31 @@ padding:10px;
 border-radius:5px
 }`;
 	document.head.appendChild(style);
-const grid = document.createElement("div");
-grid.className = "grid-container";
+const grid=document.createElement("div");
+grid.className="grid-container";
 document.body.appendChild(grid);
-
-for (let i = 1; i <= 9; i++) {
-    const gridItem = document.createElement("div");
-    gridItem.className = "grid-item";
-    gridItem.innerText = i;
-    grid.appendChild(gridItem);
+for (let i = 0; i <9; i++) {
+	const gridItem=document.createElement("div");
+	gridItem.className="grid-item";
+	gridItem.innerText=i+1;
+	grid.appendChild(gridItem);
 }
-
-const blockin = document.createElement("input");
-blockin.id = "block_id";
+const blockin=document.createElement("input");
+blockin.id="block_id";
+blockin.placeholder="enter block id"
 document.body.appendChild(blockin);
-
-const blockco = document.createElement("input");
-blockco.id = "colour_id";
+const blockco=document.createElement("input");
+blockco.id="colour_id";
+blockco.placeholder="enter your color"
 document.body.appendChild(blockco);
-
-const button = document.createElement("button");
-button.id = "change_button";
-button.innerText = "Change";
+const button=document.createElement("button");
+button.id="change_button";
+button.innerText="Change color"
 document.body.appendChild(button);
-
-const resetBtn = document.createElement("button");
-resetBtn.id = "reset_button";
-resetBtn.innerText = "Reset";
-document.body.appendChild(resetBtn);
+const buttons=document.createElement("button");
+buttons.id="Reset";
+buttons.innerText="reset button"
+document.body.appendChild(buttons);
 button.addEventListener("click", () => {
     const blockId = Number(blockin.value);
     const color = blockco.value;
@@ -71,7 +68,7 @@ button.addEventListener("click", () => {
         alert("Invalid Block ID");
     }
 });
-resetBtn.addEventListener("click", () => {
+buttons.addEventListener("click", () => {
     document.querySelectorAll(".grid-item").forEach(block => {
         block.style.backgroundColor = "";
     });
@@ -79,4 +76,3 @@ resetBtn.addEventListener("click", () => {
     blockin.value = "";
     blockco.value = "";
 });
-document.body.appendChild(buttons);
